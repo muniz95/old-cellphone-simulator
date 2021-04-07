5799eaf72c09a742a36f8053d16ee2b23f5529c0
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2021-04-07 03:06:51 -0300

    Simple clock view implemented

--- src/views/Clock.tsx
+++ src/views/Clock.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import '../styles/Home.scss';
 
 const Clock = () => {
+  const tick = () => {
+    setDate(new Date());
+  }
+  const timerID: NodeJS.Timer = setInterval(tick, 1000);
+  const [date, setDate] = React.useState(new Date());
+
+  React.useEffect(() => {
+    return () => clearInterval(timerID);
+  });
+
   return (
     <div className="home">
-      Clock View
+      <h1>{`${date.toLocaleTimeString("pt-br")}`}</h1>
     </div>
   )
 };
