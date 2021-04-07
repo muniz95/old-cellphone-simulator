cb9e8fef25caa53d48c30317c0a74700c87385a0
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2021-04-07 05:19:22 -0300

    Simple calculator implemented

---
+++ src/views/Calculator/index.tsx
@@ -0,0 +1,85 @@
+import React, { MouseEvent } from "react";
+import S from "./styled";
+
+const Calculator = () => {
+  const [expression, setExpression] = React.useState("");
+  const handleClick = (e: MouseEvent<HTMLDivElement>) => {
+    setExpression(expression => `${expression}${(e.target as HTMLDivElement).innerText}`);
+  }
+  const clear = () => setExpression("");
+  const back = () => setExpression(x => `${x.slice(0, x.length-1)}`);
+
+  const evaluate = () => {
+    const sanitizedExpression = expression.replaceAll("x", "*");
+    const result = eval(sanitizedExpression);
+    setExpression(result);
+  }
+
+  return (
+    <div className="home">
+      <div>
+        <textarea name="calculator" id="calculator" value={expression} />
+      </div>
+      <S.KeyboardContainer>
+        <S.Key onClick={clear}>
+          <div>CC</div>
+        </S.Key>
+        <S.Key/>
+        <S.Key/>
+        <S.Key onClick={back}>
+          <div>c</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>1</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>2</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>3</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>+</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>4</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>5</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>6</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>-</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>7</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>8</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>9</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>x</div>
+        </S.Key>
+        <S.Key onClick={evaluate}>
+          <div>=</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>0</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>.</div>
+        </S.Key>
+        <S.Key onClick={handleClick}>
+          <div>/</div>
+        </S.Key>
+      </S.KeyboardContainer>
+    </div>
+  )
+};
+
+export default Calculator;
