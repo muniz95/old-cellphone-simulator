479bc49d0a61d73b08e8183c8714360ddca71d26
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2023-11-26 10:09:07 -0300

    Add blinking wrapper for components

--- src/components/BatteryStatus/index.tsx
+++ src/components/BatteryStatus/index.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 import S from './styled';
+import { RootState } from '../../redux/reducers';
+import Blink from '../Blink';
 
 const BatteryStatus = () => {
+  const { isRecharging, batteryLevel } = useSelector((state: RootState) => state);
   return (
     <S.BatteryStatus className="noselect">
-      <S.StatusBar>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</S.StatusBar><br />
-      <S.StatusBar>&nbsp;&nbsp;&nbsp;&nbsp;</S.StatusBar><br />
-      <S.StatusBar>&nbsp;&nbsp;&nbsp;</S.StatusBar><br />
-      <S.StatusBar>&nbsp;&nbsp;</S.StatusBar><br />
-      <S.StatusBar>&nbsp;</S.StatusBar><br />
-              B
+      <Blink interval={1000}>
+        <S.StatusBar>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</S.StatusBar><br />
+      </Blink>
+      <Blink interval={0}>
+        <S.StatusBar>&nbsp;&nbsp;&nbsp;&nbsp;</S.StatusBar><br />
+      </Blink>
+      <Blink interval={0}>
+        <S.StatusBar>&nbsp;&nbsp;&nbsp;</S.StatusBar><br />
+      </Blink>
+      <Blink interval={0}>
+        <S.StatusBar>&nbsp;&nbsp;</S.StatusBar><br />
+      </Blink>
+      <Blink interval={0}>
+        <S.StatusBar>&nbsp;</S.StatusBar><br />
+      </Blink>
+      B
     </S.BatteryStatus>
   );
 }
---
+++ src/components/Blink/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface IProps {
+  children: React.ReactElement | React.ReactElement[]
+  interval: number
+}
+
+const Blink = ({ children, interval }: IProps) => {
+  const currentTimer = React.useRef<NodeJS.Timeout>();
+  const [isVisible, setIsVisible] = React.useState(true);
+
+  React.useEffect(() => {
+    if (interval > 0) {
+      currentTimer.current = setInterval(() => {
+        setIsVisible(state => !state);
+      }, interval);
+    } else {
+      clearInterval(currentTimer.current);
+    }
+  }, [interval]);
+
+  return (
+    isVisible ?
+    <div>
+      { children }
+    </div> :
+    <></>
+  );
+}
+
+export default Blink;
