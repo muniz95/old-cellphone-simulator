92baa733e252702b146e740c9504701798dd0ced
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2023-11-27 04:18:32 -0300

    Removed unused declarations

--- src/views/PhoneBook/PhoneBookErase/index.tsx
+++ src/views/PhoneBook/PhoneBookErase/index.tsx
@@ -3,7 +3,6 @@ import { useDispatch } from "react-redux";
 import { openModal, setThirdLevel } from "../../../redux/actions";
 import S from "./styled";
 import service from "../../../services/contact.service"
-import TextInput from "../../../components/TextInput";
 import vibration from "../../../utils/vibration";
 import { Contact } from "../../../interfaces/contact";
 
@@ -18,17 +17,13 @@ const PhoneBookErase = () => {
     [dispatch]
   );
 
-  const [name, setName] = React.useState("");
   const [contacts, setContacts] = React.useState<Contact[]>([]);
   const [currentContact, setCurrentContact] = React.useState<Contact>();
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.currentTarget.value);
-  }
   
-  const removeContact = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const removeContact = () => {
     try {
       service.removeContact({
-        name,
+        name: "",
         number: currentContact?.number!,
         id: currentContact?.id!,
         date: currentContact?.date!,
