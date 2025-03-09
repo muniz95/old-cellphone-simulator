b9f013a423d1f3bf5d69f05582029aefd1e4d568
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2025-03-08 21:20:53 -0300

    Refactor PhoneBookServiceNos component by extracting service number logic into a custom hook for improved separation of concerns and maintainability

--- src/features/phone-book/service-numbers/index.tsx
+++ src/features/phone-book/service-numbers/index.tsx
@@ -1,20 +1,19 @@
-import { useCallback, useEffect, useState } from 'react';
-import { Contact } from '../../../interfaces/contact';
-import service from '../../../services/contact.service';
-import S from './styled';
-import { setThirdLevel } from '../../../redux/actions';
+import { useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { setThirdLevel } from '../../../redux/actions';
+import S from './styled';
+import { useServiceNumbers } from './hooks/use-service-numbers';
 
 const PhoneBookServiceNos = () => {
   const dispatch = useDispatch();
-  const [contacts, setContacts] = useState<Contact[]>([]);
+  const { contacts } = useServiceNumbers();
+
   const dispatchSetThirdLevel = useCallback(
     (position: number) => dispatch(setThirdLevel(position + 1)),
     [dispatch]
   );
 
   useEffect(() => {
-    setContacts(service.getServiceNumbers());
     dispatchSetThirdLevel(0);
   }, [dispatchSetThirdLevel]);
 
