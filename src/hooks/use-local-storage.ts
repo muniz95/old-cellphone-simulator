9065577cf842ab78f95fd278d103c696403d5598
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2025-03-15 13:17:50 -0300

    Add useLocalStorage hook for managing local storage state

---
+++ src/hooks/use-local-storage.ts
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+
+const handleStoredValue = (value: string | number | boolean | object) => {
+  try {
+    if (typeof value === 'object') return JSON.stringify(value);
+    return value.toString();
+  } catch (error) {
+    console.error(error);
+    return value.toString();
+  }
+};
+
+const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      if (item) {
+        try {
+          return JSON.parse(item);
+        } catch {
+          if (item.toLowerCase() === 'true') {
+            return true;
+          } else if (item.toLowerCase() === 'false') {
+            return false;
+          }
+
+          const number = Number(item);
+          if (!isNaN(number)) {
+            return number;
+          }
+
+          return item;
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    try {
+      if (!storedValue) return;
+      window.localStorage.setItem(key, handleStoredValue(storedValue));
+    } catch (error) {
+      console.error(error);
+    }
+  }, [key, storedValue]);
+
+  return [storedValue, setStoredValue];
+};
+
+export default useLocalStorage;
