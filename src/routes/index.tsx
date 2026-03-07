64ed5ba4cf682dc383e9214dbbb04c9a53efa27f
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2026-03-07 00:38:03 -0300

    Removed unused routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,11 @@ import { RouteObject } from 'react-router-dom';
 import mainRoutes from './main';
 import { phoneBookModule } from '@/features/phone-book/module';
 import { settingsModule } from '@/features/settings/module';
-import startupRoutes from './startup';
 
 const routes: RouteObject[] = [
   ...mainRoutes,
   ...phoneBookModule.routes,
   ...settingsModule.routes,
 ];
-
-export const startupRoute: RouteObject[] = [...startupRoutes];
 
 export default routes;
--- src/routes/startup.tsx
+++
@@ -1,6 +0,0 @@
-import { RouteObject } from 'react-router-dom';
-import Startup from '@/shared/ui/startup';
-
-const routes: RouteObject[] = [{ path: '/', element: <Startup /> }];
-
-export default routes;
